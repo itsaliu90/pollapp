64790a20c3c76496b3ad20e47b201f535b94b48a
Alex Yuning Liu <yuningalexliu@gmail.com>
2014-04-27 19:56:56 -0400

    created start page, added initial links

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Pollapp::Application.routes.draw do
   root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get 'welcome/start' => 'welcome#start'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
