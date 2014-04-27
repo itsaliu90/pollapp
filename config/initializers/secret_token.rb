41df44b79302e95416cc75d8c3ccbc9adbd81449
Alex Yuning Liu <yuningalexliu@gmail.com>
2014-04-26 21:17:03 -0400

    restore to square 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pollapp::Application.config.secret_key_base = '7cc07518da5e9380a701e91121ce30feb23316e219e6659afa1014a8c8935608358ec1c107e28112eaac2b25f960f0d7994393c4a9f68986d4c5363627b0f243'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pollapp::Application.config.session_store :cookie_store, key: '_pollapp_session'
