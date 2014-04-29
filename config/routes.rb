817aef2d92badf2d156d4c848551879953959ca4
Alex Yuning Liu <yuningalexliu@gmail.com>
2014-04-28 22:58:12 -0400

    renamed routes, added numbers resource, generated numbers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Pollapp::Application.routes.draw do
-  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,13 +6,13 @@ Pollapp::Application.routes.draw do
   root 'welcome#index'
 
   # Example of regular route:
-  get 'welcome/start' => 'welcome#start'
+  get 'sign_up' => 'welcome#sign_up'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :numbers
 
   # Example resource route with options:
   #   resources :products do
