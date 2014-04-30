d0b856d05e4ecc16d7040865ce307bbf27dcc410
Alex Yuning Liu <yuningalexliu@gmail.com>
2014-04-30 19:55:25 -0400

    created working PhoneNumber model

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,2 +1,18 @@
 class NumbersController < ApplicationController
+
+def create
+	@phone_number = PhoneNumber.new(phone_number_params)
+ 
+	@phone_number.save
+	redirect_to sign_up_url
+end
+ 
+def show
+	@phone_number = PhoneNumber.find(params[:id])
+end
+
+private
+  def phone_number_params
+    params.require(:phone_number).permit(:owner, :number)
+  end
 end
